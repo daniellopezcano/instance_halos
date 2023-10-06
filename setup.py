539d416795e478f6897d4f665a657bc8ece0471f
Daniel Lopez Cano <daniellopezcano13@gmail.com>
2023-10-06 15:38:53 +0200

    fixed a name conflict in setup.py

--- setup.py
+++ setup.py
@@ -4,17 +4,17 @@ import numpy as np
 
 extensions = [
     Extension(
-        "panoptic_halos.cython_funcs",
-        sources=["panoptic_halos/cython_funcs.pyx"],
+        "instance_halos.cython_funcs",
+        sources=["instance_halos/cython_funcs.pyx"],
         include_dirs=[np.get_include()]  # Include NumPy's header files
     )
 ]
 
 setup(
-    name="panoptic_halos",
+    name="instance_halos",
     author="Daniel Lopez",
     author_email="daniellopezcano13@gmail.com",
     version="0.1.0",
     ext_modules = cythonize(extensions),
-    packages=["panoptic_halos"],
+    packages=["instance_halos"],
 )
